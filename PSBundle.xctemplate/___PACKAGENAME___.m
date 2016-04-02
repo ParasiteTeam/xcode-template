67db3cc23306a49cd6e813f3b490af3a82a8f637
Timm Kandziora <shinvou@gmail.com>
2016-04-03 00:05:03 +0200

    Cleaned up
    Removed Dock entry and the source code

--- PSBundle.xctemplate/___PACKAGENAME___.m
+++ PSBundle.xctemplate/___PACKAGENAME___.m
@@ -8,17 +8,3 @@
 
 #import <ParasiteRuntime/ParasiteRuntime.h>
 
-@interface LPRunnable : NSObject
-- (char)recentlyAdded;
-@end
-
-ZKSwizzleInterface($recentlyAdded, LPRunnable, NSObject)
-
-@implementation $recentlyAdded
-
-- (char)recentlyAdded
-{
-    return 1;
-}
-
-@end
